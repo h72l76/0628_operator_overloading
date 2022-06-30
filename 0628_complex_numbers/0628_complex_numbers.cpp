dee74b9c3e46362de89a7ff0d10cb15a469de519
hlu <huabolu@outlook.com>
2022-06-30 13:10:38 -0500

    operator overloading.

--- 0628_complex_numbers/0628_complex_numbers.cpp
+++ 0628_complex_numbers/0628_complex_numbers.cpp
@@ -11,67 +11,77 @@ public:
     Complex() = default; 
     Complex(double real, double image = 0.0)
         : _real{ real }, _image{ image } {} 
-    double getReal() const { return _real;  }
-    double getImage() const { return _image;  }
-    void setReal(double real) { _real = real;  }
-    void setImage(double image) { _image = image;  }
+    
+    // friend functions for operator overloading
+    friend Complex operator + (const Complex& c1, const Complex& c2);
+    friend Complex operator - (const Complex& c1, const Complex& c2);
+    friend Complex operator * (const Complex& c1, const Complex& c2);
+    friend Complex operator / (const Complex& c1, const Complex& c2);
+
+    friend bool operator == (const Complex& c1, const Complex& c2);
+
+    friend std::ostream& operator<< (std::ostream& out, const Complex& c);
+
 };
 
-// independent functions that use getter/setters
-Complex add(const Complex& c1, const Complex& c2) {
-    auto real = c1.getReal() + c2.getReal(); 
-    auto image = c1.getImage() + c2.getImage(); 
-    Complex result = Complex{ real, image }; 
-    return result;
-}
-
-Complex sub(const Complex& c1, const Complex& c2) { 
-    return Complex{ c1.getReal() - c2.getReal(),
-                        c1.getImage() - c2.getImage()
-    };
-}
-
-Complex mul(const Complex& c1, const Complex& c2)
-{
-    auto real = c1.getReal() * c2.getReal() - c1.getImage() * c2.getImage();
-    auto image = c1.getReal() * c2.getImage() + c1.getImage() * c2.getReal();
-
-    return Complex{ real, image };
-}
-
-Complex div(const Complex& c1, const Complex& c2)
-{
-    if (c2.getReal() == 0.0 and c2.getImage() == 0.0) {
-        std::cerr << "cannot divide zero" << std::endl;
-        exit(1);
-    }
-
-    auto conjugate = Complex{ c2.getReal(), -1 * c2.getImage() };
-    auto numerator = mul(c1, conjugate);
-    auto denominator = mul(c2, conjugate).getReal();
-
-    return Complex
-    { numerator.getReal() / denominator,
-     numerator.getImage() / denominator };
-}
-
-void print(const Complex& c) {
-    std::cout << c.getReal() << (c.getImage() >= 0? "+" : "")
-        
-        << c.getImage() << "i" << std::endl;
-}
 
 int main()
 {
     Complex c1{20, -4}; 
     Complex c2{ 3, 2 }; 
 
-    print(add(c1, c2));
-    print(sub(c1, c2));
-    print(mul(c1, c2));
-    print(div(c1, c2));
+    std::cout << c1 + c2 << std::endl;
+    std::cout << c1 - c2 << std::endl;
+    std::cout << c1 * c2 << std::endl;
+    std::cout << c1 / c2 << std::endl;
+
+    std::cout << std::boolalpha << (c1 == c1) << std::endl;
+    std::cout << std::boolalpha << (c1 == c2) << std::endl;
 
     return 0;
 }
 
+Complex operator+(const Complex& c1, const Complex& c2)
+{
+    return Complex(c1._real + c2._real, c1._image + c2._image);
+}
 
+Complex operator-(const Complex& c1, const Complex& c2)
+{
+    return Complex(c1._real - c2._real, c1._image - c2._image);
+}
+
+Complex operator*(const Complex& c1, const Complex& c2)
+{
+    return Complex{
+        c1._real * c2._real - c1._image * c2._image,
+        c1._image * c2._real + c1._real * c2._image
+    };
+}
+
+Complex operator/(const Complex& c1, const Complex& c2)
+{
+    if (c2._real == 0 && c2._image == 0) {
+        std::cerr << "cannot divide zero. " << std::endl;
+        exit(1);
+    }
+
+    auto conjugate = Complex{ c2._real, -1 * c2._image };
+    auto numerator = c1 * conjugate;
+    auto denominator = (conjugate * c2)._real;
+
+    return Complex{ numerator._real / denominator,
+                    numerator._image / denominator };
+}
+
+bool operator==(const Complex& c1, const Complex& c2)
+{
+    return c1._real == c2._real && c1._image == c2._image;
+}
+
+std::ostream& operator<<(std::ostream& out, const Complex& c)
+{
+    out << c._real << (c._image >= 0 ? "+" : "")
+        << c._image << "i";
+    return out;
+}
